6bebd7b941cf2dd7f63a860f06667317a8595322
Douglas Belinato <douglas_belinato@hotmail.com>
2020-10-13 08:38:42 -0300

    Creating orphanages mape

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import {Map} from 'react-leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg'
+
+import '../styles/pages/orphanages-map.css'
 
 function OrphanegesMap() {
     return (
-        <h1>Hello</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Santo André</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-23.6452227,-46.5442955]}
+                zoom={15}
+                style={{ width: 100%, height: 100% }}
+            >
+
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>
+        </div>
     );
 }
 
